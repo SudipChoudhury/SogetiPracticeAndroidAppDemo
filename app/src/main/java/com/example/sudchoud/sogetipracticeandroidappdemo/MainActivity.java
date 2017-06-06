5738e099c881b5603c487644bc5232ee23f273e9
SudipChoudhury <sudip.choudhury@gmail.com>
2017-06-06 15:04:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sudchoud/sogetipracticeandroidappdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sudchoud.sogetipracticeandroidappdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
